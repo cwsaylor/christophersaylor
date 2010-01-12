d867451a2664a0afffd58e7df4d570bf10deb564
Chris Saylor <cwsaylor@gmail.com>
2010-01-12 14:58:03 -0500

    Trhing out built in cache control

--- masquerade.rb
+++ masquerade.rb
@@ -5,10 +5,11 @@ set :public, Proc.new { File.join(root, "output") }
 
 # This before filter ensures that your pages are only ever served 
 # once (per deploy) by Sinatra, and then by Varnish after that
-before do
-  response.headers['Cache-Control'] = 'public, max-age=31557600' # 1 year
-end
+# before do
+#   response.headers['Cache-Control'] = 'public, max-age=31557600' # 1 year
+# end
 
 get '/' do
+  cache_control :public, :max_age => 31557600
   File.read('output/index.html')
 end
